985388b9df78c1ac2201fa5d998b6188ee47720c
bread <hi@claire.zone>
2022-09-14 00:22:43 -0500

    disable gpu

--- inkydash.sh
+++ inkydash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-firefox --headless --screenshot --window-size=600,448 file:///home/alpine/inkydash.html
+firefox --headless --screenshot --disable-gpu --window-size=600,448 file:///home/alpine/inkydash.html
 yes '' | su - root 'python3 /home/alpine/inkydash.py'
