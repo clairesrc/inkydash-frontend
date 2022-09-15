a3c86b47a9b5bf1e3e2a965b11c2f4b6d9c4eeff
bread <hi@claire.zone>
2022-09-15 00:21:00 -0500

    chrome options

--- screenshotter.py
+++ screenshotter.py
@@ -4,7 +4,11 @@ from selenium import webdriver
 
 def screenshot(url):
     ob = Screenshot.Screenshot()
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     img_url = ob.full_Screenshot(driver, save_path=r".", image_name="screenshot.png")
     driver.close()
